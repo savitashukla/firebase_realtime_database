a5820721b695fddf62e97eacd99bbe6beac4717c
savita shukla <savita@gmng.pro>
2023-10-17 13:37:24 +0530

    firebase init state

---
+++ android/app/src/main/kotlin/com/example/firebase_realtime_database/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.firebase_realtime_database
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,189 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+   await Firebase.initializeApp();
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+
+}
+
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  final databaseReference = FirebaseDatabase.instance.reference();
+
+  void _incrementCounter() {
+    createData();
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+
+    void createData(){
+      databaseReference.child("flutterDevsTeam1").set({
+        'name': 'Deepak Nishad',
+        'description': 'Team Lead'
+      });
+      databaseReference.child("flutterDevsTeam2").set({
+        'name': 'Yashwant Kumar',
+        'description': 'Senior Software Engineer'
+      });
+      databaseReference.child("flutterDevsTeam3").set({
+        'name': 'Akshay',
+        'description': 'Software Engineer'
+      });
+      databaseReference.child("flutterDevsTeam4").set({
+        'name': 'Aditya',
+        'description': 'Software Engineer'
+      });
+      databaseReference.child("flutterDevsTeam5").set({
+        'name': 'Shaiq',
+        'description': 'Associate Software Engineer'
+      });
+      databaseReference.child("flutterDevsTeam6").set({
+        'name': 'Mohit',
+        'description': 'Associate Software Engineer'
+      });
+      databaseReference.child("flutterDevsTeam7").set({
+        'name': 'Naveen',
+        'description': 'Associate Software Engineer'
+      });
+    }
+
+
+
+  void readData(){
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            const SizedBox(height: 20,),
+            GestureDetector(
+              onTap: (){
+
+             final dataV=   databaseReference.once();
+             print("dataV.toString()");
+             print(dataV.toString());
+
+              },
+              child: Container(
+                color: Colors.teal,
+                height: 50,child: const Text("data",style: TextStyle(color: Colors.pink),),),
+            ),
+
+
+            const SizedBox(height: 20,),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
